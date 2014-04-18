c51139a778f9894b1ee20e7f6868ec71228ba2c4
Guillaume Gouchon <guillaume.gouchon@gmail.com>
2014-04-18 13:06:00 +0100

    update gunrfile

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
         yeoman: {
             // Configurable paths
             app: 'app',
-            dist: '../server/public'
+            dist: '../mychrome'
         },
 
         // Watches files for changes and runs tasks based on the changed files
